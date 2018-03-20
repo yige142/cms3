0761ea88db27dc025c91d07cded82b284d77fb9b
yige142 <279290309@qq.com>
2018-03-21 00:46:35 +0800

    0.3
    首页+head

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -5,11 +5,19 @@ use think\Controller;
 
 class Index extends Controller
 {
+    //    public function index()
+    //    {
+    //        $this->assign('name','12344444');
+    //        return $this->fetch();
+    //
+    //       //return 'TP5';
+    //    }
+
     public function index()
     {
-        $this->assign('name','12344444');
+
         return $this->fetch();
 
-       //return 'TP5';
+
     }
 }
